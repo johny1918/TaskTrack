d3ed91a4bda4ad1542a3587b8f6a277222b5ba53
Ioan Andrasi <andrasi.ioan90@gmail.com>
2026-01-02 10:33:12 +0200

    implement get user by email

--- task_track/src/infra/repositories.rs
+++ task_track/src/infra/repositories.rs
@@ -1,6 +1,7 @@
-use chrono::Utc;
+use anyhow::Ok;
 use sqlx::PgPool;
 use uuid::Uuid;
+use chrono::Utc;
 
 pub struct UserRepo {
     pool: PgPool,
@@ -11,18 +12,35 @@ impl UserRepo {
         Self { pool }
     }
 
-    pub async fn insert_user(&self, email: &str, password_hash: &str) -> anyhow::Result<Uuid> {
-        let rec = sqlx::query!(
-            r#"
+    pub async fn insert_user(&self, email: &str, password_hash: &str)
+        -> anyhow::Result<Uuid> {
+            let rec= sqlx::query!(
+                r#"
                 INSERT INTO users(email,password_hash)
                 VALUES($1, $2)
                 RETURNING id
                 "#,
-            email,
-            password_hash
+                email,
+                password_hash
+            )
+            .fetch_one(&self.pool)
+            .await?;
+            Ok(rec.id)
+        }
+
+    pub async fn get_user_by_email(&self, email: &str) -> anyhow::Result<Option<(uuid::Uuid, String)>> {
+        let rec = sqlx::query!(
+            r#"SELECT id, password_hash FROM users WHERE email = $1"#,
+            email
         )
-        .fetch_one(&self.pool)
+        .fetch_optional(&self.pool)
         .await?;
-        Ok(rec.id)
+        
+        if let Some(r) = rec {
+            Ok(Some((r.id, r.password_hash)))
+        }else {
+            Ok(None)
+        }
     }
+
 }
